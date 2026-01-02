b74d429481166ef4fb5066e7e143b522c0aa6616
Suhas Moon <suhasmoon11@gmail.com>
2026-01-02 14:02:46 +0530

    Add the comments

--- Youtube-FrontEnd/src/pages/Trending.jsx
+++ Youtube-FrontEnd/src/pages/Trending.jsx
@@ -1,3 +1,14 @@
+/**
+ * Trending Component
+ * ----------------------------------------------------------
+ * This is a demo page that represents the "Trending" section
+ * similar to YouTube's trending videos page.
+ * 
+ * Features:
+ * - Displays a heading for trending videos
+ * - Optional description below the heading
+ * - Can be extended later to fetch actual trending videos
+ */
 const Trending = () => {
   return (
     <div className="p-6 text-xl font-semibold">
